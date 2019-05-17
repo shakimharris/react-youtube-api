d685239c70631f5a64a7c841fc2f000df90c0f90
shakimharris <amenraprod@gmail.com>
2019-05-17 01:24:52 -0400

    added axios method to get response

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,14 @@ import SearchBar from './SearchBar';
 class App extends React.Component {
 
 onSearchSubmit (term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/', {
+    params: { query: term},
+    headers: {
+        Authorization: 'Client-ID f70d590add5c282efa55738697eea438f3f56ec4547a5a818ba6ab3fda5875fe'
+    }
+    }).then((response) =>  {
+      console.log(response.data.results);
+    });
 }
 render () {
   return (
