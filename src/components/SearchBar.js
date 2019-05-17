8053cc86f469694269a2d7c45c53f016fa753049
shakimharris <amenraprod@gmail.com>
2019-05-16 23:28:30 -0400

    added changemethod for text input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+ onInputChange() {
+
+ }
+
+
     render(){
      return(
         <div className="ui segment">
          <form className="ui form">
           <div class="field">
            <label>Search Images</label>
-             <input type="text" />
+             <input type="text" onChange={this.onInputChange} />
              </div>
               </form>
 
