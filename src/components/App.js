3189b8d2945e98f046e81d0531bd3be8227142ac
shakimharris <amenraprod@gmail.com>
2019-05-19 11:56:40 -0400

    setstate to image results

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ async onSearchSubmit (term) {
         Authorization: 'Client-ID f70d590add5c282efa55738697eea438f3f56ec4547a5a818ba6ab3fda5875fe'
     }
     });
-    console.log(response.data.results);
+    this.setState({ images: response.data.results});
 }
 render () {
   return (
