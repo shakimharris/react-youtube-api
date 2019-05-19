8af90568cbcba5dd7a06d1652eb55d369ddcdff9
shakimharris <amenraprod@gmail.com>
2019-05-19 14:33:52 -0400

    added alt

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ImageList = props => {
  const images = props.images.map(image => {
-    return <img key={image.id} src={image.urls.regular} />;
+    return <img alt ={image.description} key={image.id} src={image.urls.regular} />;
 });
 
     return <div>{images}</div>;
