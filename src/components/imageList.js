92760288b2b3a68d03a8981cc53db97adbb0299d
shakimharris <amenraprod@gmail.com>
2019-05-19 15:29:26 -0400

    removed old args

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,10 +1,11 @@
 import './imageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 
 const ImageList = props => {
  const images = props.images.map(({ description, id, urls }) => {
-    return <img alt ={description} key={id} src={urls.regular} />;
+    return <ImageCard key={id} />;
 });
 
     return <div className="image-list">{images}</div>;
