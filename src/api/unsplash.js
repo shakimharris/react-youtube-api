470fa66f6886ee2b1b2ed8eb774f6a745dc9ec01
shakimharris <amenraprod@gmail.com>
2019-05-19 12:18:13 -0400

    exported function for splash get config

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.create({
+export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: 'Client-ID f70d590add5c282efa55738697eea438f3f56ec4547a5a818ba6ab3fda5875fe'
