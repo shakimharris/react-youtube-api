9bec8d0e3dd2b171a4c0853dd0dcf4c311b6cf44
shakimharris <amenraprod@gmail.com>
2019-05-19 15:32:36 -0400

    removed key destructure

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -4,8 +4,8 @@ import ImageCard from './ImageCard';
 
 
 const ImageList = props => {
- const images = props.images.map(({ description, id, urls }) => {
-    return <ImageCard key={id} />;
+ const images = props.images.map((image) => {
+    return <ImageCard key={image.id} image={image} />;
 });
 
     return <div className="image-list">{images}</div>;
