dc23ff711a4ddf5ac9034cca456ae11d233a722d
shakimharris <amenraprod@gmail.com>
2019-05-19 12:50:44 -0400

    create image reference variable

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ImageList = props => {
- props.image.map((image) => {
+ const images = props.image.map((image) => {
     return <img src={image.urls.regular} />
 });
 
-    return <div>ImageList</div>;
+    return <div>{images}</div>;
 
 };
 
